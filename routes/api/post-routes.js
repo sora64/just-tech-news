7a51d8fa4ca33fb5e2a806be7662a321e0ac661f
Alan Phelps <phelpsa64@gmail.com>
2022-02-24 19:39:07 -0500

    can now create and manipulate comments on post data

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { User, Post, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require("../../models");
 
 // get all users
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
-        // Query configuration
+        order: [['created_at', 'DESC']],
         attributes: [
             'id',
             'post_url',
@@ -14,8 +14,15 @@ router.get('/', (req, res) => {
             'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
-        order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -42,6 +49,14 @@ router.get('/:id', (req, res) => {
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
