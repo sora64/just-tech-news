da23e53f8608b1be967bf7b6f6b78408d1183ad4
Alan Phelps <phelpsa64@gmail.com>
2022-03-01 20:27:16 -0500

    created a single post page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,35 +1,35 @@
-const router = require("express").Router();
-const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   console.log(req.session);
 
   Post.findAll({
     attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
+      'id',
+      'post_url',
+      'title',
+      'created_at',
       [
         sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
         ),
-        "vote_count",
+        'vote_count',
       ],
     ],
     include: [
       {
         model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ["username"],
+          attributes: ['username'],
         },
       },
       {
         model: User,
-        attributes: ["username"],
+        attributes: ['username'],
       },
     ],
   })
@@ -37,7 +37,10 @@ router.get("/", (req, res) => {
       // pass a single post object into the homepage template
       // console.log(dbPostData[0]);
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("homepage", { posts });
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -45,50 +48,50 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/login", (req, res) => {
+router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/");
+    res.redirect('/');
     return;
   }
 
-  res.render("login");
+  res.render('login');
 });
 
-router.get("/post/:id", (req, res) => {
+router.get('/post/:id', (req, res) => {
   Post.findOne({
     where: {
       id: req.params.id,
     },
     attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
+      'id',
+      'post_url',
+      'title',
+      'created_at',
       [
         sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
         ),
-        "vote_count",
+        'vote_count',
       ],
     ],
     include: [
       {
         model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ["username"],
+          attributes: ['username'],
         },
       },
       {
         model: User,
-        attributes: ["username"],
+        attributes: ['username'],
       },
     ],
   })
     .then((dbPostData) => {
       if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
+        res.status(404).json({ message: 'No post found with this id' });
         return;
       }
 
@@ -96,7 +99,10 @@ router.get("/post/:id", (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render("single-post", { post });
+      res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch((err) => {
       console.log(err);
--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,15 +1,15 @@
 async function logout() {
-    const response = await fetch('/api/users/logout', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' }
-    });
-    console.log('Pikachu', response);
+  const response = await fetch('/api/users/logout', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  console.log('Pikachu', response);
 
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert(response.statusText);
-    }
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
 }
 
 document.querySelector('#logout').addEventListener('click', logout);
